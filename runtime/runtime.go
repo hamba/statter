3ac187d6cd529ba63bac5a0737bf5906cde53f25
Nicholas Wiersma <nick@wiersma.co.za>
2022-07-13 06:14:54 +0200

    fix: GC pause runtime stats (#75)

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -40,37 +40,41 @@ func newRuntimeStats() *runtimeStats {
 }
 
 func (r *runtimeStats) send(s *statter.Statter) {
+	ms := r.MemStats
+
 	// CPU
 	s.Gauge("runtime.cpu.goroutines").Set(float64(r.goroutines))
 
 	// Memory
 	// General
-	s.Gauge("runtime.memory.alloc").Set(float64(r.MemStats.Alloc))
-	s.Gauge("runtime.memory.total").Set(float64(r.MemStats.TotalAlloc))
-	s.Gauge("runtime.memory.sys").Set(float64(r.MemStats.Sys))
-	s.Gauge("runtime.memory.lookups").Set(float64(r.MemStats.Lookups))
-	s.Gauge("runtime.memory.mallocs").Set(float64(r.MemStats.Mallocs))
-	s.Gauge("runtime.memory.frees").Set(float64(r.MemStats.Frees))
+	s.Gauge("runtime.memory.alloc").Set(float64(ms.Alloc))
+	s.Gauge("runtime.memory.total").Set(float64(ms.TotalAlloc))
+	s.Gauge("runtime.memory.sys").Set(float64(ms.Sys))
+	s.Gauge("runtime.memory.lookups").Set(float64(ms.Lookups))
+	s.Gauge("runtime.memory.mallocs").Set(float64(ms.Mallocs))
+	s.Gauge("runtime.memory.frees").Set(float64(ms.Frees))
 
 	// Heap
-	s.Gauge("runtime.memory.heap.alloc").Set(float64(r.MemStats.HeapAlloc))
-	s.Gauge("runtime.memory.heap.sys").Set(float64(r.MemStats.HeapSys))
-	s.Gauge("runtime.memory.heap.idle").Set(float64(r.MemStats.HeapIdle))
-	s.Gauge("runtime.memory.heap.inuse").Set(float64(r.MemStats.HeapInuse))
-	s.Gauge("runtime.memory.heap.objects").Set(float64(r.MemStats.HeapObjects))
-	s.Gauge("runtime.memory.heap.released").Set(float64(r.MemStats.HeapReleased))
+	s.Gauge("runtime.memory.heap.alloc").Set(float64(ms.HeapAlloc))
+	s.Gauge("runtime.memory.heap.sys").Set(float64(ms.HeapSys))
+	s.Gauge("runtime.memory.heap.idle").Set(float64(ms.HeapIdle))
+	s.Gauge("runtime.memory.heap.inuse").Set(float64(ms.HeapInuse))
+	s.Gauge("runtime.memory.heap.objects").Set(float64(ms.HeapObjects))
+	s.Gauge("runtime.memory.heap.released").Set(float64(ms.HeapReleased))
 
 	// Stack
-	s.Gauge("runtime.memory.stack.inuse").Set(float64(r.MemStats.StackInuse))
-	s.Gauge("runtime.memory.stack.sys").Set(float64(r.MemStats.StackSys))
-	s.Gauge("runtime.memory.stack.mcache_inuse").Set(float64(r.MemStats.MCacheInuse))
-	s.Gauge("runtime.memory.stack.mcache_sys").Set(float64(r.MemStats.MCacheSys))
-	s.Gauge("runtime.memory.stack.mspan_inuse").Set(float64(r.MemStats.MSpanInuse))
-	s.Gauge("runtime.memory.stack.mspan_sys").Set(float64(r.MemStats.MSpanSys))
+	s.Gauge("runtime.memory.stack.inuse").Set(float64(ms.StackInuse))
+	s.Gauge("runtime.memory.stack.sys").Set(float64(ms.StackSys))
+	s.Gauge("runtime.memory.stack.mcache_inuse").Set(float64(ms.MCacheInuse))
+	s.Gauge("runtime.memory.stack.mcache_sys").Set(float64(ms.MCacheSys))
+	s.Gauge("runtime.memory.stack.mspan_inuse").Set(float64(ms.MSpanInuse))
+	s.Gauge("runtime.memory.stack.mspan_sys").Set(float64(ms.MSpanSys))
 
 	// GC
-	s.Gauge("runtime.memory.gc.last").Set(float64(r.MemStats.LastGC))
-	s.Gauge("runtime.memory.gc.next").Set(float64(r.MemStats.NextGC))
-	s.Gauge("runtime.memory.gc.count").Set(float64(r.MemStats.NumGC))
-	s.Timing("runtime.memory.gc.pause").Observe(time.Duration(r.MemStats.PauseTotalNs))
+	s.Gauge("runtime.memory.gc.last").Set(float64(ms.LastGC))
+	s.Gauge("runtime.memory.gc.next").Set(float64(ms.NextGC))
+	s.Gauge("runtime.memory.gc.count").Set(float64(ms.NumGC))
+	s.Gauge("runtime.memory.gc.pause_total").Set(float64(ms.PauseTotalNs))
+	pauseNs := ms.PauseNs[(ms.NumGC+255)%256]
+	s.Timing("runtime.memory.gc.pause").Observe(time.Duration(pauseNs))
 }
